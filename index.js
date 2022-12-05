81e826e16636017ed7e43f8364037af966b7c7f6
Zeynab Hamad <zaynabhamad950@gmail.com>
2022-12-05 20:52:18 +0300

    compleated

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(z){
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  if(z <= 400){
+    return 'This one is on me!';
+  }
+  else if(z >400 && z <2000){
+    return 'That will be twenty bucks.';
+  }
+  else if(z > 2000 && z <2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else{
+    return 'No can do.';
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+}
+// -------------------------------------------------
+
+
+function ternaryCheckCity(city){
+
+  if(city == "NYC"){
+    return 'Ok, sounds good.';
+  }
+  else{
+    return 'No go.';
+  }
+
+}
+// -------------------------------------------------
+
+
+function switchOnCharmFromTip(a){
+
+  if(a =="generous"){
+    return 'Thank you so much.';
+  }
+  else if(a =="not as generous"){
+    return 'Thank you.';
+  }
+  else{
+    return 'Bye.';
+  }
+
+
+
+
+
 }
